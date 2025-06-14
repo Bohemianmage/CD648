d044548f30c3c9607a729be9037e0160fd8ffb7a
Bohemianmage <jeanclaudes_1992@hotmail.com>
2025-06-13 23:00:35 -0600

    Revisión de favicon

--- vite.config.js
+++ vite.config.js
@@ -5,28 +5,33 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'CD648',
-        short_name: 'CD648',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#1c1c1c',
-        theme_color: '#1c1c1c',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ],
-      }
-    })
+VitePWA({
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'CD648',
+    short_name: 'CD648',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1c1c1c',
+    theme_color: '#1c1c1c',
+   icons: [
+  {
+    src: '/favicon.svg',
+    sizes: 'any',
+    type: 'image/svg+xml'
+  },
+  {
+    src: '/cd648_icon-192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: '/cd648_icon-512.png',
+    sizes: '512x512',
+    type: 'image/png'
+  }
+]
+  }
+})
   ]
 })
